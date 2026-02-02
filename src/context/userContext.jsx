b38cfebfc53ec2a7656b98f14ca7c62e75f79299
elgar <juancarlosgarcia.an@gmail.com>
2026-02-02 23:03:06 +0100

    fixet: loging

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 
 const UserContext = createContext();
 
@@ -6,23 +6,14 @@ export const UserProvider = ({ children }) => {
     // Estado para el login del usuario
     const [userLogged, setUserLogged] = useState(false);
 
-    useEffect(() => {
-        const stored = localStorage.getItem("userLogged");
-        if (stored === "true") {
-            setUserLogged(true);
-        }
-    }, []);
-
     // Función para hacer login (simulado)
     const login = () => {
         setUserLogged(true);
-        localStorage.setItem("userLogged", "true");
     };
 
     // Función para hacer logout
     const logout = () => {
         setUserLogged(false);
-        localStorage.removeItem("userLogged");
     };
 
     return (
